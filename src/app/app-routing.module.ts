539a9ad802668a23fdae3173f21c42d0e35f666a
arcadio2 <arcadiolg2@gmail.com>
2023-04-02 21:49:57 -0600

    componentes iniciales

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './shared/pages/home/home.component';
 
 const routes: Routes = [
+  {
+    path:'home',
+    component:HomeComponent
+  },
   {
     path:'auth',
     loadChildren: ()=>import('./auth/auth.module').then(m=>m.AuthModule)
   },
   {
-    path:'**',redirectTo:'auth'
+    path:'**',redirectTo:'home'
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { HomeComponent } from './shared/pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
