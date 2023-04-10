d447836cb2a98d7b1b036b5031288002334ec15d
Ulises Miranda <ulimirandachavez@hotmail.com>
2023-04-09 18:23:18 -0600

    perfil avance

--- src/app/logged/pages/perfil/perfil.component.ts
+++ src/app/logged/pages/perfil/perfil.component.ts
@@ -1,15 +1,70 @@
 import { Component, OnInit } from '@angular/core';
-
+import { zip } from 'rxjs';
+import {Router} from "@angular/router";
 @Component({
   selector: 'app-perfil',
   templateUrl: './perfil.component.html',
   styleUrls: ['./perfil.component.css']
 })
 export class PerfilComponent implements OnInit {
+  usuario: Usuario  = {
+    id: '',
+    correo: "",
+    nombre: "",
 
-  constructor() { }
+  }
+
+  isDatoCompletos:boolean = false
+
+  constructor(
+    private router: Router
+  ) {
+  }
 
   ngOnInit(): void {
+    this.loadData()
+  }
+
+  loadData(){
+    //this.usuario = usuarioMock1
+    this.usuario = usuarioMock2
+    const numAtributos = Object.keys(this.usuario).length
+    if (numAtributos > 3){
+      this.isDatoCompletos = true
+    }
+  }
+
+  irCompletarPerfil(){
+    this.router.navigate(['completar-perfil'])
   }
 
 }
+
+interface Usuario{
+  id: string;
+  correo: string;
+  nombre: string;
+  edad?: number;
+  sexo?: string;
+  altura?: number;
+  tallaSuperior?: string;
+  tallaInferior?: string;
+}
+
+const usuarioMock1 : Usuario = {
+  id: 'JuanGL',
+  correo: "juan_garcia@gmail.com",
+  nombre: "Juan Garcia",
+  edad: 18,
+  sexo: "Masculino",
+  altura: 170,
+  tallaSuperior: 'M',
+  tallaInferior: '32'
+}
+const usuarioMock2 : Usuario = {
+  id: 'JuanGL',
+  correo: "juan_garcia@gmail.com",
+  nombre: "Juan Garcia",
+
+}
+
--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input, OnChanges } from '@angular/core';
 import { AuthService } from 'src/app/auth/services/auth.service';
 import { User } from 'src/app/models/user.model';
+import {Router} from "@angular/router";
+import {ToastrService} from "ngx-toastr";
 
 @Component({
   selector: 'app-navbar',
@@ -9,7 +11,11 @@ import { User } from 'src/app/models/user.model';
 })
 export class NavbarComponent implements OnInit, OnChanges{
 
-  constructor(private authService: AuthService) { }
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private toast:ToastrService
+  ) { }
 
   @Input() user!:User;
 
@@ -22,21 +28,30 @@ export class NavbarComponent implements OnInit, OnChanges{
       this.auth=v;
       this.user = this.authService.usuario;
     })
-    
+
   }
   ngOnChanges() {
     console.log("xd")
   }
-  
+
 
   logout(){
-    this.auth=false; 
+    this.auth=false;
   }
   login(){
-    this.auth=true; 
+    this.auth=true;
   }
 
+  irCarritoCompras(){
+    if (this.auth){
+      this.router.navigate(['user/carrito-compras'])
 
+    }
+    else{
+      this.toast.error("Inicia sesion");
+      this.router.navigate(['auth/iniciar-sesion'])
+    }
+  }
 
 
 }
