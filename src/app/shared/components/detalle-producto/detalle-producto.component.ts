5aaff522b66bcd2a108200e3c8854eeb2f028815
arcadio2 <arcadiolg2@gmail.com>
2023-04-26 21:54:28 -0600

    implementación de imagenes de productos

--- src/app/shared/components/detalle-producto/detalle-producto.component.ts
+++ src/app/shared/components/detalle-producto/detalle-producto.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit,Input } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
-import { Producto } from 'src/app/models/producto.model';
+import { DetalleProducto, Producto } from 'src/app/models/producto.model';
 import { ProductosService } from 'src/app/services/productos.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-detalle-producto',
@@ -14,13 +15,26 @@ export class DetalleProductoComponent implements OnInit {
 
   @Input()
   producto!:Producto; 
+  color!:string; 
+  nombre!:string; 
+  url_backend:string =  environment.urlBase;
 
   constructor(private route: ActivatedRoute,
     private toastService: ToastrService,
     private productoService:ProductosService) { }
 
   ngOnInit(): void {
+    this.color = this.producto.detalle![0].color?.color || '';  
+    this.nombre = this.producto.nombre || '';  
     
+  }
+
+  imagen(){
+    const url = this.url_backend; 
+    const product = this.producto; 
+
+    
+    //return`${{url}}imagenes/uploads/img/${{product?.nombre!}}/{{producto?.detalle[0]?.color`
   }
 
 
