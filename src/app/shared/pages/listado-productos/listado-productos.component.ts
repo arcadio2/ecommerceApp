21913d6bf74ac917daf337c4de60e75277ba126a
Ulises Miranda <ulimirandachavez@hotmail.com>
2023-04-30 14:19:22 -0600

    modificaciones

--- src/app/shared/pages/listado-productos/listado-productos.component.ts
+++ src/app/shared/pages/listado-productos/listado-productos.component.ts
@@ -11,18 +11,20 @@ import { ProductosService } from 'src/app/services/productos.service';
 })
 export class ListadoProductosComponent implements OnInit {
 
-  nombreProducto!:string; 
-  categoriaProducto!:string; 
-  generoProducto!:string; 
+  nombreProducto!:string;
+  categoriaProducto:string = "Ver todo";
+  generoProducto!:string;
+  catalagoMujer: string[] = CatalagoRopaMujer
+  catalagoHombre: string[] = CatalagoRopaHombre
 
   productos:Producto[]=[];
-  
+
   constructor(private route: ActivatedRoute,
     private toastService: ToastrService,
     private productoService:ProductosService) { }
 
   ngOnInit(): void {
-    
+
     this.route.queryParamMap.subscribe(params=>{
       this.nombreProducto = params.get('producto')!;
       this.categoriaProducto = params.get('categoria')!;
@@ -32,15 +34,42 @@ export class ListadoProductosComponent implements OnInit {
     });
   }
   buscarProductos(){
-    this.productoService.getSimilar1(this.nombreProducto,this.categoriaProducto,this.generoProducto).subscribe(resp=>{
-      console.log(resp.productos);
-      this.productos = resp.productos;
-    },err=>{
-      if(err.status==404){  
-        this.toastService.error("No existe el proucto")
 
-      }
-     
-    })
+    if (this.categoriaProducto === 'Ver todo'){
+      this.productoService.getSimilar1(this.nombreProducto,'null',this.generoProducto).subscribe(resp=>{
+        console.log(resp.productos);
+        this.productos = resp.productos;
+      },err=>{
+        if(err.status==404){
+          this.toastService.error("No existe el producto")
+
+        }
+
+      })
+    } else {
+      this.productoService.getSimilar1(this.nombreProducto,this.categoriaProducto,this.generoProducto).subscribe(resp=>{
+        console.log(resp.productos);
+        this.productos = resp.productos;
+      },err=>{
+        if(err.status==404){
+          this.toastService.error("No existe el producto")
+
+        }
+
+      })
+    }
+  }
+
+  seleccionarCateogoria(categoriaSeleccionada:string){
+    this.categoriaProducto = categoriaSeleccionada
+    this.buscarProductos()
   }
 }
+
+const CatalagoRopaMujer: string[] = [
+  "Ver todo", "Playeras", "Blusas", "Pantalones", "Vestidos", "Tenis", "Sueteres"
+]
+
+const CatalagoRopaHombre: string[] = [
+  "Ver todo", "Playeras", "Pantalones", "Camisas", "Tenis", "Sueteres"
+]
