c4e67ac9b9e22b708588e72b1145054458173743
Lance-24 <lancelote.ps@gmail.com>
2023-05-01 19:00:38 -0500

    completar-perfil commit Lance

---
+++ src/app/logged/pages/confirmar-compra/confirmar-compra.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-confirmar-compra',
+  templateUrl: './confirmar-compra.component.html',
+  styleUrls: ['./confirmar-compra.component.css']
+})
+export class ConfirmarCompraComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
