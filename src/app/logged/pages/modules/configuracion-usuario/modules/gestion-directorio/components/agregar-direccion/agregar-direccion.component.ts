0fecdbba964038d3fdf52ccbd7a6862234992057
Ulises Miranda <ulimirandachavez@hotmail.com>
2023-05-07 17:44:50 -0600

    modificaciones

---
+++ src/app/logged/pages/modules/configuracion-usuario/modules/gestion-directorio/components/agregar-direccion/agregar-direccion.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-agregar-direccion',
+  templateUrl: './agregar-direccion.component.html',
+  styleUrls: ['./agregar-direccion.component.css']
+})
+export class AgregarDireccionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/logged/pages/modules/configuracion-usuario/modules/gestion-directorio/components/gestionar-directorio/gestionar-directorio.component.ts
+++ src/app/logged/pages/modules/configuracion-usuario/modules/gestion-directorio/components/gestionar-directorio/gestionar-directorio.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {MatDialog} from "@angular/material/dialog";
+import {AgregarDireccionComponent} from "../agregar-direccion/agregar-direccion.component";
+import {ToastrService} from "ngx-toastr";
+import {EditarDireccionComponent} from "../editar-direccion/editar-direccion.component";
 
 @Component({
   selector: 'app-gestionar-pedidos',
@@ -6,10 +10,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./gestionar-directorio.component.css']
 })
 export class GestionarDirectorioComponent implements OnInit {
+  loading = false
 
-  constructor() { }
+  constructor(
+    private dialog: MatDialog,
+    private toastService:ToastrService
+  ) { }
 
   ngOnInit(): void {
+    this.loadData()
+  }
+
+  loadData(){
+
+  }
+
+  agregarNuevaDireccion(){
+    this.loading = true
+    const dialogRef = this.dialog.open(AgregarDireccionComponent, {
+    }).afterClosed().subscribe((res) => {
+      this.loading = false
+      if (res === true) {
+        this.loadData();
+        this.toastService.success("Dirección agregada exitosamente")
+      }
+    });
+  }
+  editarDireccion(){
+    this.loading = true
+    const dialogRef = this.dialog.open(EditarDireccionComponent, {
+    }).afterClosed().subscribe((res) => {
+      this.loading = false
+      if (res === true) {
+        this.loadData();
+        this.toastService.success("Dirección editada exitosamente")
+      }
+    });
   }
 
 }
