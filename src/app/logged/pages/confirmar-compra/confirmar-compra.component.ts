ed7593660d879e0dad4ddd02a379da33ec6ee9d8
Ulises Miranda <ulimirandachavez@hotmail.com>
2023-05-29 23:17:48 -0600

    confirmacion de la compra

--- src/app/logged/pages/confirmar-compra/confirmar-compra.component.ts
+++ src/app/logged/pages/confirmar-compra/confirmar-compra.component.ts
@@ -1,7 +1,6 @@
 import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
 import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 import {STEPPER_GLOBAL_OPTIONS} from "@angular/cdk/stepper";
-import {CrearComentarioComponent} from "../../../shared/pages/crear-comentario/crear-comentario.component";
 import {MatDialog} from "@angular/material/dialog";
 import {AuthService} from "../../../auth/services/auth.service";
 import {ToastrService} from "ngx-toastr";
@@ -11,38 +10,42 @@ import {PaymentService} from "../../../services/payment.service";
 import {StripeCardComponent, StripeService} from "ngx-stripe";
 import {StripeCardElementOptions, StripeElementsOptions} from "@stripe/stripe-js";
 import {Direcciones} from "../../../models/user.model";
-import { ComprasService } from 'src/app/services/compras.service';
-import { Router } from '@angular/router';
+import {ComprasService} from 'src/app/services/compras.service';
+import {Router} from '@angular/router';
 import {Compra} from "../../../models/compras.model";
 
 @Component({
   selector: 'app-confirmar-compra',
   templateUrl: './confirmar-compra.component.html',
   styleUrls: ['./confirmar-compra.component.css'],
-  providers: [
-    {
-      provide: STEPPER_GLOBAL_OPTIONS,
-      useValue: {showError: true},
-    },
-  ],
+  providers: [{
+    provide: STEPPER_GLOBAL_OPTIONS, useValue: {showError: true},
+  },],
 })
 export class ConfirmarCompraComponent implements OnInit, AfterViewInit {
 
-  isLinear = false;
+  isLinear = true;
   direccionForm = this.createDireccionForm();
   facturacionForm = this.createFacturacionForm()
-  productos:DetalleDto[]=[];
-  bolsa:Bolsa[]=[];
+  productos: DetalleDto[] = [];
+  bolsa: Bolsa[] = [];
   compras: Compra[] = []
   loading = false
 
   productosCompra?: ProductosCompra
   precioTotal?: number = 1000;
 
-  direccion!:Direcciones;
+  direccion!: Direcciones;
 
   error: any
   errorDomicilio: boolean = false
+  isErrorFacturacion: boolean = false
+  errorFacturacion: string = ''
+
+  municipios?: string[];
+  estadoSeleccionado?: string;
+  municipioSeleccionado?: string;
+  estados: Estado[] = estados
 
   @ViewChild(StripeCardComponent) card?: StripeCardComponent;
 
@@ -69,21 +72,21 @@ export class ConfirmarCompraComponent implements OnInit, AfterViewInit {
   constructor(
     private _formBuilder: FormBuilder,
     private dialog: MatDialog,
-    private router:Router,
-    private comprasService:ComprasService,
-    private toastService:ToastrService,
+    private router: Router,
+    private comprasService: ComprasService,
+    private toastService: ToastrService,
     private paymentService: PaymentService,
     private stripeService: StripeService,
-    private authService:AuthService
+    private authService: AuthService
   ) {
 
   }
 
   ngOnInit(): void {
-    this.productos  = this.comprasService.productos;
+    this.productos = this.comprasService.productos;
     this.bolsa = this.comprasService.bolsa;
-    console.log("A comprar",this.bolsa);
-    if(this.productos.length==0){
+    console.log("A comprar", this.bolsa);
+    if (this.productos.length == 0) {
       this.router.navigateByUrl('/listado?genero=hombre&categoria=Ver%20todo')
     }
     window.scrollTo(0, 0);
@@ -93,61 +96,45 @@ export class ConfirmarCompraComponent implements OnInit, AfterViewInit {
     window.scrollTo(0, 0);
   }
 
-  loadData(){
+  loadData() {
 
+  }
+
+  cargarMunicipios(): void {
+    const estado = this.estados?.find(e => e.nombre === this.direccionForm.controls.estado.value);
+    if (estado) {
+      this.municipios = estado.municipios;
+    } else {
+      this.municipios = [];
+    }
   }
 
   createDireccionForm() {
     return new FormGroup({
       estado: new FormControl('', {
-        nonNullable: true,
-        validators: [Validators.required]
-      }),
-      municipio: new FormControl('', {
-        nonNullable: true,
-        validators: [Validators.required]
-      }),
-      colonia: new FormControl('', {
-        nonNullable: true,
-        validators: [Validators.required]
-      }),
-      codigoPostal: new FormControl('', {
-        nonNullable: true,
-        validators: [Validators.required, Validators.pattern('[0-9]+$')]
-      }),
-      calle: new FormControl('', {
-        nonNullable: true,
-        validators: [Validators.required]
-      }),
-      numExt: new FormControl('', {
-        nonNullable: true,
-        validators: [Validators.required, Validators.pattern('[0-9]+$')]
-      }),
-      numInt: new FormControl('', {
-        nonNullable: true,
-        validators: [Validators.pattern('[0-9]+$')]
+        nonNullable: true, validators: [Validators.required]
+      }), municipio: new FormControl('', {
+        nonNullable: true, validators: [Validators.required]
+      }), colonia: new FormControl('', {
+        nonNullable: true, validators: [Validators.required]
+      }), codigoPostal: new FormControl('', {
+        nonNullable: true, validators: [Validators.required, Validators.pattern('^[0-9]{5}')]
+      }), calle: new FormControl('', {
+        nonNullable: true, validators: [Validators.required]
+      }), numExt: new FormControl('', {
+        nonNullable: true, validators: [Validators.required]
+      }), numInt: new FormControl('', {
+        nonNullable: true, validators: []
       }),
 
     })
   }
+
   createFacturacionForm() {
     return new FormGroup({
       nombreTitular: new FormControl('', {
-        nonNullable: true,
-        validators: [Validators.required]
-      })/*,
-      noTarjeta: new FormControl('', {
-        nonNullable: true,
-        validators: [Validators.required]
-      }),
-      vencimiento: new FormControl( '', {
-        nonNullable: true,
-        validators: [Validators.required]
-      }),
-      cvv: new FormControl('', {
-        nonNullable: true,
-        validators: [Validators.required]
-      })*/
+        nonNullable: true, validators: [Validators.required, Validators.pattern('^[a-zA-Z\\sáéíóúÁÉÍÓÚüÜñÑ]+$')]
+      })
     })
   }
 
@@ -164,12 +151,14 @@ export class ConfirmarCompraComponent implements OnInit, AfterViewInit {
     });
   }
 
-  guardarDireccion(){
+  guardarDireccion() {
     this.loading = true
     this.direccionForm.markAllAsTouched()
-    if (this.direccionForm.valid){
-      this.errorDomicilio =false
-      const direccionUsuario: Direcciones ={
+    this.direccionForm.disabled
+
+    if (this.direccionForm.valid) {
+      this.errorDomicilio = false
+      const direccionUsuario: Direcciones = {
         calle: this.direccionForm.controls.calle.value,
         estado: this.direccionForm.controls.estado.value,
         municipio: this.direccionForm.controls.municipio.value,
@@ -179,97 +168,125 @@ export class ConfirmarCompraComponent implements OnInit, AfterViewInit {
         cp: parseInt(this.direccionForm.controls.codigoPostal.value)
       }
       this.direccion = direccionUsuario;
-      console.log(direccionUsuario)
-      this.loading =false
-    }
-    else{
+      this.loading = false
+      this.direccionForm.enabled
+    } else {
       this.errorDomicilio = true
       this.loading = false
+      this.direccionForm.enabled
+    }
+  }
+
+  confirmarDatosPago() {
+    this.facturacionForm.markAllAsTouched()
+    if (this.facturacionForm.valid) {
+
+      const name = this.facturacionForm.controls.nombreTitular.value
+
+      this.loading = true
+      this.stripeService.createToken(this.card!.element, {name}).subscribe((result) => {
+        if (result.error) {
+          this.isErrorFacturacion = true
+          this.errorFacturacion = result.error.message ?? ''
+          this.toastService.error(result.error.message )
+        }
+        this.loading = false
+        this.isErrorFacturacion = false
+      }, _ => {
+        this.loading = true
+      })
+
     }
   }
 
   comprar(): void {
-    this.guardarDireccion()
     this.direccionForm.markAllAsTouched()
-    if (this.direccionForm.valid) {
-      this.loading = true
+    this.facturacionForm.markAllAsTouched()
+    if (this.direccionForm.valid && this.facturacionForm.valid) {
+
       const name = this.facturacionForm.controls.nombreTitular.value;
-      console.log("nombre", name)
-      this.stripeService
-        .createToken(this.card!.element, { name })
-        .subscribe((result) => {
-          if (result.token) {
-            // Use the token
-            console.log(result.token.id);
+      this.loading = true
 
-            let compra: Compra
-            let i = 0
-            const fechaHoy: Date = new Date();
-            let descripcion = ""
+      this.stripeService.createToken(this.card!.element, {name}).subscribe((result) => {
+        if (result.token) {
 
-            this.bolsa.forEach((c,index)=>{
-              let compra: Compra = {
-                //Id: id,
-                direccion: this.direccion,
-                usuario: this.authService.usuario,
-                fecha_compra: fechaHoy,
-                codigo_seguimiento: result.token.id,
-                detalle_producto: c.detalle_producto!,
-                cantidad: c.cantidad!,
-                active: true,
-                precio: this.cantidadProductos(this.productos[i].producto?.precio!,c.cantidad!)
-              }
-              console.log(compra)
-              this.compras?.push(compra)
-              descripcion += c.detalle_producto?.nombre_producto + ", "
-              i++
-            })
+          let i = 0
+          const fechaHoy: Date = new Date();
+          let descripcion = ""
 
+          this.bolsa.forEach((c, index) => {
+            let compra: Compra = {
+              direccion: this.direccion,
+              usuario: this.authService.usuario,
+              fecha_compra: fechaHoy,
+              codigo_seguimiento: result.token.id,
+              detalle_producto: c.detalle_producto!,
+              cantidad: c.cantidad!,
+              active: true,
+              precio: this.cantidadProductos(this.productos[i].producto?.precio!, c.cantidad!)
+            }
+            this.compras?.push(compra)
+            descripcion += c.detalle_producto?.nombre_producto + ", "
+            i++
+          })
 
+          const paymentIntentDto: PaymentIntentDto = {
+            token: result.token.id,
+            amount: this.precioTotal!,
+            currency: 'MXN',
+            descripcion: descripcion
+          };
 
-            const paymentIntentDto: PaymentIntentDto = {
-              token: result.token.id,
-              amount: this.precioTotal!,
-              currency: 'MXN',
-              descripcion: descripcion
-            };
-            console.log("objeto", paymentIntentDto)
+          this.paymentService.pagar(paymentIntentDto).subscribe({
+            next: (data: any) => {
+              this.irConfirmarPago(this.compras!, data.id)
+              this.loading = false
+            }, error: err => {
+              this.loading = false
+            }
+          });
 
-            this.paymentService.pagar(paymentIntentDto).subscribe({
-              next:( data:any) =>{
-                console.log(data)
-                console.log("compras", this.compras)
-                this.irConfirmarPago(this.compras!, data.id)
-                this.loading = false
-              }, error: err => {
-                console.log("hubo un error", err)
-                this.loading = false
-              }
-            });
-
-            this.error = undefined
-          } else if (result.error) {
-            // Error creating the token
-            this.error = result.error.message;
-          }
-        });
+          this.error = undefined
+        } else if (result.error) {
+          // Error creating the token
+          this.error = result.error.message;
+          this.loading = false
+        }
+      }, _ =>{
+        this.loading = false
+      });
     }
   }
 
-  cantidadProductos(precio:number,cantidad:number){
-    return cantidad*precio;
+  cantidadProductos(precio: number, cantidad: number) {
+    return cantidad * precio;
   }
 
-  calcularTotal(){
+  calcularTotal() {
     let total = 0;
 
-    this.bolsa.forEach((c,index)=>{
-      total+= c.cantidad! * this.productos[index].producto?.precio!;
+    this.bolsa.forEach((c, index) => {
+      total += c.cantidad! * this.productos[index].producto?.precio!;
 
     })
     return total;
   }
 
+  irMenuPrincipal(){
+    this.router.navigateByUrl('')
+  }
+
+}
+
+const estados: Estado[] = [
+  { nombre: 'Estado de México', municipios: ['Ecatepec', 'Coacalco', 'Toluca'] },
+  { nombre: 'Ciudad de México', municipios: ['Miguel Hidalgo', 'Itztapalapa', 'Xochimilco'] },
+  // ... más estados
+];
+
+interface Estado {
+  nombre: string
+  municipios: string[]
 }
 
 
