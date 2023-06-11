64e3a5ca5e02f3af622da1da4627cccdaa2719e8
Ulises Miranda <ulimirandachavez@hotmail.com>
2023-06-11 16:51:54 -0600

    mejoras reestablecer

--- src/app/auth/pages/reestablecer-contrasenia/reestablecer-contrasenia.component.ts
+++ src/app/auth/pages/reestablecer-contrasenia/reestablecer-contrasenia.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 import {AuthService} from "../../services/auth.service";
 import {ToastrService} from "ngx-toastr";
 import { Router } from '@angular/router';
@@ -12,27 +12,31 @@ import { Router } from '@angular/router';
 export class ReestablecerContraseniaComponent implements OnInit {
 
   email:string='';
-  contraForm!: FormGroup;
+  contraForm = new FormGroup({
+    email: new FormControl<string>('', [Validators.required, Validators.pattern('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$')])
+  })
+  loading = false
+
   constructor(private authService:AuthService,
               private router:Router,
               private fromBuilder:FormBuilder,
               private toastService:ToastrService) { }
 
   ngOnInit(): void {
-    this.contraForm = this.fromBuilder.group({
-      email:['',[Validators.required,Validators.email]]
-    })
   }
 
   restablecer(){
-    this.email = this.contraForm.get('email')?.value;
-    if(this.email!=null){
 
+    if(this.contraForm.valid){
+      this.loading = true
+      this.email = this.contraForm.controls.email.value!
       this.authService.restablecerContra(this.email).subscribe((resp:any)=>{
         this.toastService.info(resp.mensaje);
-        this.router.navigateByUrl('/auth'); 
+        this.router.navigateByUrl('/auth');
+        this.loading = false
       },error=>{
         this.toastService.error("El email no es válido o no está registrado");
+        this.loading = false
       })
     }
 
